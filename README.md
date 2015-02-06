24a191f992f7898416e690e7c45b6b2ee7cf5563
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-02-06 20:27:36 +0100

    Added functions for computing the mean and variance of a sample

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # Statistics
 
-The repository hosts four packages:
+The package provides functions used in mathematical statistics.
+
+## [Documentation][doc]
+
+## Nested Packages
+
+The repository hosts five nested packages:
 
 * [correlation](correlation),
 * [decomposition](decomposition),
@@ -13,3 +19,5 @@ The repository hosts four packages:
 1. Fork the project.
 2. Implement your idea.
 3. Create a pull request.
+
+[doc]: http://godoc.org/github.com/ready-steady/statistics
---
+++ main.go
@@ -0,0 +1,27 @@
+// Package statistics provides functions used in mathematical statistics.
+package statistics
+
+// Mean computes an estimate of the population mean from a finite sample.
+func Mean(data []float64) float64 {
+	sum := 0.0
+
+	for _, x := range data {
+		sum += x
+	}
+
+	return sum / float64(len(data))
+}
+
+// Variance computes an estimate of the population variance from a finite
+// sample. The estimate is unbiased.
+func Variance(data []float64) float64 {
+	n, μ := float64(len(data)), Mean(data)
+
+	Σ1, Σ2 := 0.0, 0.0
+	for _, x := range data {
+		Σ1 = Σ1 + (x-μ)*(x-μ)
+		Σ2 = Σ2 + (x - μ)
+	}
+
+	return (Σ1 - Σ2*Σ2/n) / (n - 1)
+}
