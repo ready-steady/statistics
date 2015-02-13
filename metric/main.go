546ac5787b81e1737c16426a7c9af7e8c9f74a58
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-02-13 12:46:19 +0100

    Cosmetic renamings in metric

--- metric/main.go
+++ metric/main.go
@@ -9,42 +9,42 @@ import (
 // MSE computes the mean-square error.
 //
 // https://en.wikipedia.org/wiki/Mean_squared_error
-func MSE(predicted, observed []float64) float64 {
+func MSE(predictions, observations []float64) float64 {
 	var sum, Δ float64
 
-	for i := range observed {
-		Δ = predicted[i] - observed[i]
+	for i := range observations {
+		Δ = predictions[i] - observations[i]
 		sum += Δ * Δ
 	}
 
-	return sum / float64(len(observed))
+	return sum / float64(len(observations))
 }
 
 // RMSE computes the root-mean-square error.
 //
 // https://en.wikipedia.org/wiki/Root-mean-square_deviation
-func RMSE(predicted, observed []float64) float64 {
-	return math.Sqrt(MSE(predicted, observed))
+func RMSE(predictions, observations []float64) float64 {
+	return math.Sqrt(MSE(predictions, observations))
 }
 
 // NRMSE computes the normalized root-mean-square error.
 //
 // https://en.wikipedia.org/wiki/Root-mean-square_deviation#Normalized_root-mean-square_deviation
-func NRMSE(predicted, observed []float64) float64 {
-	count := len(observed)
+func NRMSE(predictions, observations []float64) float64 {
+	count := len(observations)
 	if count == 0 {
 		return 0
 	}
 
-	min, max := observed[0], observed[0]
+	min, max := observations[0], observations[0]
 	for i := 1; i < count; i++ {
-		if observed[i] < min {
-			min = observed[i]
+		if observations[i] < min {
+			min = observations[i]
 		}
-		if observed[i] > max {
-			max = observed[i]
+		if observations[i] > max {
+			max = observations[i]
 		}
 	}
 
-	return RMSE(predicted, observed) / (max - min)
+	return RMSE(predictions, observations) / (max - min)
 }
