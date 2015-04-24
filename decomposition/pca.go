f47f0503cfe71829f91632f3677f17309286e9a4
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-04-24 10:46:38 +0200

    decomposition: update the usage of linear/decomposition

--- decomposition/pca.go
+++ decomposition/pca.go
@@ -3,21 +3,23 @@ package decomposition
 import (
 	"errors"
 
-	"github.com/ready-steady/linear/matrix"
+	"github.com/ready-steady/linear/decomposition"
 )
 
 // CovPCA performs principal component analysis on an m-by-m covariance matrix
-// Σ. The principal components U and their variances Λ are returned in
-// descending order of the variances. By definition, the variances should be
-// nonnegative. Due to finite-precision arithmetics, however, some close-to-zero
-// variances might turn out to be negative. If the absolute value of a negative
-// variance is smaller than the tolerance ε, the function nullifies that
-// variance and proceeds without any errors; otherwise, an error is returned.
+// Σ. The principal components U and their variances Λ are returned in the
+// descending order of the variances.
+//
+// By definition, the variances should be nonnegative. Due to finite-precision
+// arithmetics, however, some close-to-zero variances might turn out to be
+// negative. If the absolute value of a negative variance is smaller than the
+// tolerance ε, the function nullifies that variance and proceeds without any
+// errors; otherwise, an error is returned.
 func CovPCA(Σ []float64, m uint, ε float64) (U []float64, Λ []float64, err error) {
 	U = make([]float64, m*m)
 	Λ = make([]float64, m)
 
-	if err = matrix.SymmetricEigen(Σ, U, Λ, m); err != nil {
+	if err = decomposition.SymmetricEigen(Σ, U, Λ, m); err != nil {
 		return nil, nil, err
 	}
 
@@ -31,7 +33,7 @@ func CovPCA(Σ []float64, m uint, ε float64) (U []float64, Λ []float64, err er
 		}
 	}
 
-	// NOTE: Λ is in ascending order. Reverse!
+	// Λ is in the ascending order. Reverse!
 	for i, j := uint(0), m-1; i < j; i, j = i+1, j-1 {
 		Λ[i], Λ[j] = Λ[j], Λ[i]
 		for k := uint(0); k < m; k++ {
