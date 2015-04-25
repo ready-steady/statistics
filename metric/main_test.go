8f163bdbe627fe218ed24a2b84d592f523200e4c
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-04-25 07:24:09 +0200

    metric: add a benchmark for detect

--- metric/main_test.go
+++ metric/main_test.go
@@ -2,6 +2,7 @@ package metric
 
 import (
 	"math"
+	"math/rand"
 	"sort"
 	"testing"
 
@@ -24,4 +25,21 @@ func TestUnique(t *testing.T) {
 	sort.Float64s(data)
 
 	assert.Equal(data, []float64{0, 1, 2, 3, 4, 5}, t)
+}
+
+func BenchmarkDetect(b *testing.B) {
+	rand.Seed(0)
+	data1 := random(1e6, 10)
+	data2 := random(1e6, 10)
+	for i := 0; i < b.N; i++ {
+		detect(data1, data2)
+	}
+}
+
+func random(count, max uint) []float64 {
+	data := make([]float64, count)
+	for i := range data {
+		data[i] = float64(uint(float64(max) * rand.Float64()))
+	}
+	return data
 }
