9da520633c96b62079d1e24efdfe58a7c449c8ff
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-04-26 21:50:20 +0200

    Make use of the Kahan summation algorithm

--- distribution/moment.go
+++ distribution/moment.go
@@ -1,18 +1,28 @@
 package distribution
 
-// Mean computes an estimate of the population mean from a finite sample.
+// Mean computes an estimate of the population mean from a finite sample. The
+// computation is based on the Kahan summation algorithm.
+//
+// https://en.wikipedia.org/wiki/Kahan_summation_algorithm
 func Mean(data []float64) float64 {
-	sum := 0.0
+	c := 0.0
 
+	Σ := 0.0
 	for _, x := range data {
-		sum += x
+		y := x - c
+		t := Σ + y
+		c = (t - Σ) - y
+		Σ = t
 	}
 
-	return sum / float64(len(data))
+	return Σ / float64(len(data))
 }
 
 // Variance computes an estimate of the population variance from a finite
-// sample. The estimate is unbiased.
+// sample. The estimate is unbiased. The computation is based on the
+// compensated-summation version of the two-pass algorithm.
+//
+// https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance#Two-pass_algorithm
 func Variance(data []float64) float64 {
 	n, μ := float64(len(data)), Mean(data)
 
