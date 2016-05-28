d45d9ee97a75e96c930ade8222c5a75aa40ae021
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-05-28 09:26:22 +0200

    regression: fix the comments

--- regression/linear.go
+++ regression/linear.go
@@ -1,19 +1,21 @@
 package regression
 
-/// SimpleLinear is a simple linear regression model.
+// SimpleLinear is a simple linear regression model.
+//
+// https://en.wikipedia.org/wiki/Simple_linear_regression
 type SimpleLinear struct {
 	α float64
 	β float64
 }
 
-/// NewSimpleLinear fits a simple linear regression model.
+// NewSimpleLinear fits a simple linear regression model.
 func NewSimpleLinear(x, y []float64) *SimpleLinear {
 	α := 0.0
 	β := 0.0
 	return &SimpleLinear{α, β}
 }
 
-/// Compute evaluate the model at a point.
+// Compute evaluate the model at a point.
 func (self *SimpleLinear) Compute(x float64) float64 {
 	return self.α*x + self.β
 }
